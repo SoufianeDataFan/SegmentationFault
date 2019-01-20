c94af0d9efea3443727baa64b959577f651b41f2
CHAMI Soufiane <chami.soufiane.fr@gmail.com>
2019-01-19 18:30:30 -0600

    Create script_sample.py

---
+++ script_sample.py
@@ -0,0 +1,40 @@
+
+param = {'num_leaves': 120,
+         'objective':'regression',
+         'max_depth': -1,
+         'learning_rate': 0.01,
+         "min_child_samples": 30,
+         "boosting": "gbdt",
+         "feature_fraction": 0.9,
+         "bagging_freq": 1,
+         "bagging_fraction": 0.9 ,
+         "bagging_seed": 11,
+         "metric": 'rmse',
+         "lambda_l1": 0.1,
+         "verbosity": -1,
+         'device' :'gpu',
+         'gpu_platform_id': 0,
+         'gpu_device_id' : 0,
+         "random_state": 4590}
+
+folds = StratifiedKFold(n_splits=5, shuffle=True, random_state=4590)
+oof = np.zeros(len(df_train))
+predictions = np.zeros(len(df_test))
+feature_importance_df = pd.DataFrame()
+
+for fold_, (trn_idx, val_idx) in enumerate(folds.split(df_train,df_train['outliers'].values)):
+    print("fold {}".format(fold_))
+    trn_data = lgb.Dataset(df_train.iloc[trn_idx][df_train_columns], label=target.iloc[trn_idx])#, categorical_feature=categorical_feats)
+    val_data = lgb.Dataset(df_train.iloc[val_idx][df_train_columns], label=target.iloc[val_idx])#, categorical_feature=categorical_feats)
+
+    num_round = 10000
+    clf = lgb.train(param, trn_data, num_round, valid_sets = [trn_data, val_data], verbose_eval=100, early_stopping_rounds = 100)
+    oof[val_idx] = clf.predict(df_train.iloc[val_idx][df_train_columns], num_iteration=clf.best_iteration)
+    
+    fold_importance_df = pd.DataFrame()
+    fold_importance_df["Feature"] = df_train_columns
+    fold_importance_df["importance"] = clf.feature_importance()
+    fold_importance_df["fold"] = fold_ + 1
+    feature_importance_df = pd.concat([feature_importance_df, fold_importance_df], axis=0)
+    
+    predictions += clf.predict(df_test[df_train_columns], num_iteration=clf.best_iteration) / folds.n_splits
